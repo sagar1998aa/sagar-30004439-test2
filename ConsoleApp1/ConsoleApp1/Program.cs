4be82dbd970dd4ff2682787d4135797c53ac2291
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2019-05-22 11:13:58 +1200

    test2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿/*sagar devkota*/
+/* student id=30004439*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class salary
+    {
+        private Double Annual;//setting instance variables
+        private Double Weekely;
+
+        public double Annual//settint get/get
+         get {return annual;}
+         set {annual=value;}
+     }
+
+   {     
+         public Double Weekely;
+         get{return weekely}
+         set{weekely=ValueType;}
+    }
+
+    {
+        public salary {double_Annual, double_weekely}
+    }
+    {
+
+    Annual=_annual;
+        Weekely=_weekely;
+     }
+
+    public string Income()
+{
+    return "/nyour yuearly income is $" + Annual +"/nyour weekely pay is"$ +Weekely 
+}
+
+class Wage
+{
+    private Double hourly;//setting instance variables
+    private Double Weekely;
+    private int num;
+    private Double weekelywages;
+
+    public double hourly;//settint get/get
+    {
+    get {return hourley }
+    set {hourly=value}
+}
+      public double num;
+    {
+    get {return num }
+    set {hourley=value}
+}
+       public double weekelywage;
+    {
+    get {return weekelywage }
+    set {weekelywage=value}
+}
+    public Wage(double _hourley,int_num, double _weekelywage)
+{
+
+
+    Hourley = _hourley;
+    Num = _num;
+    Weekelywage = _weekelywage;
+}
+
+public string wage()
+{
+
+
+}
+static void Main(string[] args)
+        {
+            Console.WriteLine("Payroll system");
+            double salary = 80000;
+            double wage = 33.72;
+
+            Salary s1 = new salary(annual weekely);
+            Console.WriteLine("s1.Income");
+            Console.WriteLine("/nyour yearly salary is: $(s1.Annual per year./ nyour weekely incomei is: $(s1.weekely)per week");
+
+
+      
+        }
+    }
+}
